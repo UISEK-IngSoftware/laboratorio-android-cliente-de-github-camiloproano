7d06944fb765fe2563f475a59f192e1206f627c3
laboratorio.fs <camilo.proano@uisek.edu.ec>
2025-10-29 12:03:50 -0500

    Arreglos y añadidos de repositorios.

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,8 +1,17 @@
 package ec.edu.uisek.githubclient
 
 import android.os.Bundle
+import android.security.advancedprotection.AdvancedProtectionManager
+import android.view.textclassifier.ConversationActions
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
+import ec.edu.uisek.githubclient.services.GithubApiService
+import ec.edu.uisek.githubclient.services.RetrofitClient
+import retrofit2.Response
+import retrofit2.Call
+import retrofit2.Callback
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,5 +29,39 @@ class MainActivity : AppCompatActivity() {
     private fun setupRecyclerView() {
         reposAdapter = ReposAdapter()
         binding.reposRecyclerView.adapter = reposAdapter
+    }
+
+    private fun fetchRepositories(){
+        val apiService: GithubApiService = RetrofitClient.githubApiService
+        val call = apiService.getRepos()
+
+        call.enqueue(object: Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>?>, response: Response<List<Repo>?>) {
+                if(response.isSuccessful){
+                    val repos = response.body()
+                    if (response != null && repos.isNotEmpty()) {
+                        reposAdapter.updateRepositories(repos)
+                    } else {
+                        showMessage("No se encontraron repositorios")
+                    }
+                } else {
+                    val errorMessage = when(response.code()) {
+                        401 -> "No autorizado"
+                        403 -> "Prohibido"
+                        404 -> "No encontrado"
+                        else -> "Error ${response.code()}"
+                    }
+                    showMessage("Error: $errorMessage")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repo>?>, t: Throwable) {
+                showMessage("No se pudieron cargar los repositorios")
+            }
+        })
+    }
+
+    private fun showMessage (message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
